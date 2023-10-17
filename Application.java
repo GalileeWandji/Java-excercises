b06af0156e9f431853f4d9e4f260f4d2f1039c35
GalileeWandji <146034249+GalileeWandji@users.noreply.github.com>
2023-10-17 23:53:52 +0100

    Add files via upload

--- Application.java
+++ Application.java
@@ -1,9 +1,27 @@
 package Application;
 
-public class Application{
+public class Application {
+	
+	public static boolean App(int k, int [] numbers) {
+	
+		for(int i=0; i<numbers.length; i++)
+		{
+			for(int j=i+1; j<numbers.length; i++)
+			{
+				
+				if(numbers[i] + numbers[j] == k) 
+					return true;
+				}
+			}
+
+			return false;
+	
+}
 	public static void main(String[] args) {
-		for(int i=2; i<=20; i+=2) {
-			System.out.println(i);
-		}
+		int [] num = {2,4,6,8};
+		System.out.println(App(10, num));
+
+		
 	}
+
 }
